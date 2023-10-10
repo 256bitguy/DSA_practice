7cf05f7b882d9f30b20ba69b97958b4c1cd34db9
256bitguy <ashishrajsri2233@gmail.com>
2023-10-11 01:57:04 +0530

    added new code

--- assignment41/q4.cpp
+++ assignment41/q4.cpp
@@ -2,28 +2,30 @@
 // Example:
 // Input: str = “I Love To Code”
 // Output: Code To Love I
-#include<iostream>
-#include<stack>
-#include<string>
+#include <iostream>
+#include <stack>
+#include <string>
 using namespace std;
-int main(){
-    string s="I Love To Code";
-     stack<string> st;
-     
-     string s1="";
-     for(char c: s){
-        if(c!=' ') s1+=c;
-         if(c==' '||c=='\0'){
-          
-            st.push(s1);
-            s1="";
 
-         }
-     }
-     while(!st.empty()){
-        string ch=st.top();
+int main() {
+    string s = "I Love To Code";
+    stack<string> st;
+
+    string s1 = "";
+    for (char c : s) {
+        if (c != ' ')
+            s1 += c;
+        if (c == ' ' || c == '\0') {
+            if (!s1.empty()) { // Check if s1 is not empty before pushing
+                st.push(s1);
+                s1 = "";
+            }
+        }
+    }
+    while (!st.empty()) {
+        string ch = st.top();
         st.pop();
-        cout<<ch<<" ";
-     }
-     return 0;
+        cout << ch << " ";
+    }
+    return 0;
 }
